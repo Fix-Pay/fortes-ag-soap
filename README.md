af8f8c957ae3640d179977eb67a26510b6f877bb
Rafael Pinheiro <rafaeleupinheiro@gmail.com>
2022-04-20 10:37:12 -0300

    update project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# fortes-ag-soap
+# api-fortes-ag - Repositório referente ao manual de integração com o Fortes Financeiro (WEB SERVICE)
+
+## Baixar 
+```shell script
+$ go get github.com/Fix-Pay/fortes-ag-soap
+```
---
+++ fortes-ag-soap.go
@@ -0,0 +1,39 @@
+package fortes_ag_soap
+
+import (
+	"encoding/xml"
+	"github.com/Fix-Pay/gowsdl/soap"
+	"time"
+)
+
+var _ time.Time
+var _ xml.Name
+
+type RequestClientExample struct {
+	ConsiderarCancelados     bool   `xml:"ConsiderarCancelados,omitempty" json:"ConsiderarCancelados,omitempty"`
+	CPFCNPJ                  string `xml:"CPFCNPJ,omitempty" json:"CPFCNPJ,omitempty"`
+	DetalhaContatosEServicos bool   `xml:"DetalhaContatosEServicos,omitempty" json:"DetalhaContatosEServicos,omitempty"`
+}
+
+type IAGExample interface {
+	GetListaClientesExample(request *RequestClientExample) (*string, error)
+}
+
+type iAGExample struct {
+	client *soap.Client
+}
+
+func (i *iAGExample) GetListaClientesExample(request *RequestClientExample) (*string, error) {
+	response := new(string)
+	err := i.client.Call("urn:AGIntf-IAG#getListaClientes", request, response)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+func NewIAGExample(client *soap.Client) IAGExample {
+	return &iAGExample{
+		client: client,
+	}
+}
