12e92a944dc2d00c40249c66cad430f8a6254a55
Armando Couto <coutoarmando@gmail.com>
2022-05-16 13:13:46 -0300

    Ajuste

--- servicos.go
+++ servicos.go
@@ -24,21 +24,7 @@ type GetListaServicosResponse struct {
 }
 
 type ServicoFortes struct {
-	Codigo         string  `json:"codigo"`
-	Nome           string  `json:"nome"`
-	NomeApresentar string  `json:"nome_apresentar"`
-	Valor          float64 `json:"valor"`
-	Iss            int     `json:"iss"`
-	Inss           int     `json:"inss"`
-	Irrf           int     `json:"irrf"`
-	PisCofinsCsll  int     `json:"pis_cofins_csll"`
-	AliqISS        float64 `json:"aliq_iss"`
-	AliqINSS       float64 `json:"aliq_inss"`
-	AliqIRRF       float64 `json:"aliq_irrf"`
-	NomeMunicipio  float64 `json:"nomemunicipio"`
-	AliqPIS        float64 `json:"aliq_pis"`
-	AliqCOFINS     float64 `json:"aliq_cofins"`
-	AliqCSL        float64 `json:"aliq_csl"`
+	Item []string `xml:"item"`
 }
 
 func (i *iAG) GetListaServicos(request *GetListaServicosRequest, response *EnvelopeResponse) error {
